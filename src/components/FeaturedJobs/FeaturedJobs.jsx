5e9cbc9350cff9be1be3a55e5d0e88cc8467ba39
Fahmida Faiza <fahmidafaiza57@gmail.com>
2023-09-23 00:05:00 +0600

    loaded data

---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import Job from "../Job/Job";
+
+
+const FeaturedJobs = () => {
+
+const [jobs, setjobs] = useState([]);
+
+
+
+
+
+
+useEffect(() =>{
+  fetch('jobs.json')
+  .then(res => res.json())
+  .then(data => setjobs(data));
+},[])
+
+
+
+
+    return (
+        <div>
+            <div className="text-center">
+                <h2 className="text-5xl">Featured Jobs: {jobs.length}</h2>
+                <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+
+
+            <div>
+
+                {
+                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedJobs;
